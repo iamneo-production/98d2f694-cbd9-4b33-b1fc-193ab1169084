785797fad7988b12c7babe5d2f5eea375f32d57d
gopi26 <gopirogopi@gmail.com>
2022-03-08 10:33:54 +0000

    angularapp

---
+++ springapp/src/main/java/com/example/BookstoreApplication.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+// @CrossOrigin(origins = "https://8081-cbccabcededbabffafaadcdfeceadbceedce.examlyiopb.examly.io")
+
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+//	@SuppressWarnings("deprecation")
+	@Bean
+   public WebMvcConfigurer corsConfigurer() {
+       return new WebMvcConfigurerAdapter() {
+           @Override
+           public void addCorsMappings(CorsRegistry registry) {
+               registry.addMapping("/**")
+               .allowedHeaders("Access-control-Allow-Origin")
+               .allowedHeaders("Access-Control-Allow-Headers")
+               .allowedHeaders("Access-Control-Allow-Credentials")
+               .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+               .allowedOrigins("*");
+           }
+       };
+   }
+
+}
